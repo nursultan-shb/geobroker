d262e714f2fa5c19620fb8bb058217168f7d6ee8
Nursultan <collisioncourse@mail.ru>
2020-03-11 11:28:53 +0100

    removed the unused code.

--- GeoBroker-Server/src/main/java/de/hasenburg/geobroker/server/loadAnalysis/ResourceMetrics.java
+++ GeoBroker-Server/src/main/java/de/hasenburg/geobroker/server/loadAnalysis/ResourceMetrics.java
@@ -2,17 +2,28 @@ package de.hasenburg.geobroker.server.loadAnalysis;
 
 import de.hasenburg.geobroker.commons.model.message.Topic;
 import de.hasenburg.geobroker.commons.model.message.loadbalancer.TopicMetrics;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * ResourceMetrics is a thread-safe class used to store topic metrics.
+ *
+ * @author Nursultan
+ * @version 1.0
+ */
+
 public class ResourceMetrics {
     static ConcurrentHashMap<Topic, Integer> publishedMessages = new ConcurrentHashMap<>();
-    private static final Logger logger = LogManager.getLogger();
 
+    /**
+     * Sets a number of published messages for a topic, i.e., topic/number of published messages mapping.
+     *
+     * @param topic         A topic.
+     * @param messagesCount a number of published messages
+     * @return void
+     */
     public synchronized static void setPublishedMessages(Topic topic, Integer messagesCount) {
         if (publishedMessages.containsKey(topic)) {
             Integer mc = publishedMessages.get(topic);
@@ -22,21 +33,23 @@ public class ResourceMetrics {
         }
     }
 
-    public synchronized static List<TopicMetrics> getPublishedMessages(String brokerId){
+    /**
+     * Gets all topic/published messages mappings as a list of TopicMetrics.
+     *
+     * @param brokerId The ID of a current broker.
+     * @return List of TopicMetrics
+     */
+    public synchronized static List<TopicMetrics> getPublishedMessages(String brokerId) {
         List<TopicMetrics> tm = new ArrayList<>();
-        publishedMessages.forEach((k,v) -> tm.add(new TopicMetrics(brokerId, k.getTopic(), v)));
+        publishedMessages.forEach((k, v) -> tm.add(new TopicMetrics(brokerId, k.getTopic(), v)));
         return tm;
     }
 
-
-    public synchronized static String getPublishedMessagesAsString() {
-        StringBuilder sb = new StringBuilder();
-        publishedMessages.forEach((k, v) -> sb.append(String.format(k + "=" + v + "|")));
-        String result = sb.length() > 0 ? sb.substring(0, sb.length() - 1).trim() : "";
-
-        return result;
-    }
-
+    /**
+     * Clears the topic/published messages map.
+     *
+     * @return void
+     */
     public synchronized static void clear() {
         publishedMessages.clear();
     }
